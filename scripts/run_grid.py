a24bf924a5bebac7ed1e210947489b9bdf2b800e
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-06-05 18:19:09 -0400

    Added submission script

---
+++ scripts/run_grid.py
@@ -0,0 +1,15 @@
+from trustbutverify.datasets import forcefields, water_models, targets
+from simtk import unit as u
+import itertools
+import sys
+
+rank = int(sys.argv[1])
+
+for k, ff, water, target in enumerate(itertools.product(forcefields, water_models, targets)):
+
+    if k != rank:
+        continue
+
+    target.build(ff, water)
+    target.equilibrate(ff, water)
+    target.production(ff, water)
