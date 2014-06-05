1cd4da28759ab72e6964f58d8449c9f47b5dcfb6
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-06-05 14:28:24 -0400

    Updated driver

--- tmp/driver.py
+++ tmp/driver.py
@@ -4,12 +4,15 @@ import itertools
 
 forcefields = ["amber10", "amber03"]
 water_models = ["tip3p", "tip3pfb"]
-targets = ["1vii", "1am7", "1ubq"]
 
 temperature = 300 * u.kelvin
 
+targets = []
+targets.append(protein_system.ProteinSystem("1vii", temperature))
+targets.append(protein_system.ProteinSystem("1am7", temperature))
+targets.append(protein_system.ProteinSystem("1ubq", temperature))
+
 for ff, water, target in itertools.product(forcefields, water_models, targets):
-    target = protein_system.ProteinSystem(target, temperature)
     target.build(ff, water)
     target.equilibrate(ff, water)
     target.production(ff, water)
