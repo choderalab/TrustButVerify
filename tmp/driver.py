6f8e8244801c0b6013136de796a4e91cc4a48ab7
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-06-05 13:19:47 -0400

    Updated driver

--- tmp/driver.py
+++ tmp/driver.py
@@ -1,12 +1,15 @@
 from trustbutverify import protein_target
 from simtk import unit as u
+import itertools
 
-ff = "amber10"
-water = "tip3p"
-pdbid = "1vii"
+forcefields = ["amber10", "amber03"]
+water_models = ["tip3p", "tip3pfb"]
+targets = ["1vii", "1am7", "1ubq"]
+
 temperature = 300 * u.kelvin
 
-target = protein_target.ProteinTarget(pdbid, temperature)
-target.build(ff, water)
-target.equilibrate(ff, water)
-target.production(ff, water)
+for ff, water, target in itertools.product(forcefields, water_models, targets):
+    target = protein_target.ProteinTarget(pdbid, temperature)
+    target.build(ff, water)
+    target.equilibrate(ff, water)
+    target.production(ff, water)
