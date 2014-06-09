afbc59b42f78926f4cde56fc579a4cc029622cf9
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-06-09 11:15:43 -0400

    Separate run and analyze

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,6 +1,23 @@
-from trustbutverify.datasets import forcefields, water_models, targets
+from trustbutverify.datasets import forcefields, water_models, targets, all_analyzers
+from trustbutverify.analyzers import accumulate_experiments, BuzzScalarCouplingAnalyzer, OhScalarCouplingAnalyzer
 from simtk import unit as u
 import itertools
+import pandas as pd
 
-for k, (ff, water, target) in enumerate(itertools.product(forcefields, water_models, targets)):
-    t = target.load(ff, water)
+expt = accumulate_experiments(all_analyzers)
+
+predictions = []
+for ff, water, target in itertools.product(forcefields, water_models, targets):
+    predictions.append(target.analyze(ff, water, all_analyzers))
+
+predictions = pd.concat(predictions)
+
+predictions["expt"] = expt.ix[predictions.identifier].values
+predictions["delta"] = predictions["value"] - predictions["expt"]
+predictions["delta2"] = predictions["delta"] ** 2
+
+predictions = predictions.dropna()
+
+rms = (predictions.groupby(["ff", "water"]).delta2).mean() ** 0.5
+
+
--- scripts/run_serial.py
+++ scripts/run_serial.py
@@ -1,26 +1,12 @@
-from trustbutverify.datasets import forcefields, water_models, targets, all_analyzers
-from trustbutverify.analyzers import accumulate_experiments, BuzzScalarCouplingAnalyzer, OhScalarCouplingAnalyzer
+from trustbutverify.datasets import forcefields, water_models, targets
+from trustbutverify.analyzers import accumulate_experiments
 from simtk import unit as u
 import itertools
 import pandas as pd
 
 expt = accumulate_experiments(all_analyzers)
 
-predictions = []
 for ff, water, target in itertools.product(forcefields, water_models, targets):
     target.build(ff, water)
     target.equilibrate(ff, water)
     target.production(ff, water)
-    predictions.append(target.analyze(ff, water, all_analyzers))
-
-predictions = pd.concat(predictions)
-
-predictions["expt"] = expt.ix[predictions.identifier].values
-predictions["delta"] = predictions["value"] - predictions["expt"]
-predictions["delta2"] = predictions["delta"] ** 2
-
-predictions = predictions.dropna()
-
-rms = (predictions.groupby(["ff", "water"]).delta2).mean() ** 0.5
-
-
