b7c71ae5c244b415ac48c9c88194ef46ce206559
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-06-28 18:33:48 -0400

    Moved print statement in grid run

--- scripts/run_grid.py
+++ scripts/run_grid.py
@@ -6,10 +6,9 @@ import sys
 rank = int(sys.argv[1])
 
 for k, (ff, water, target) in enumerate(itertools.product(forcefields, water_models, targets)):
-    print(k, ff, water, target.identifier)
     if k != rank:
         continue
-
+    print(k, ff, water, target.identifier)
     target.build(ff, water)
     target.equilibrate(ff, water)
     target.production(ff, water)
