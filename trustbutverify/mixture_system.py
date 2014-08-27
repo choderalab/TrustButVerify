e19d347bd7768dd64754be9bc985b3b9d5d226d4
Julie Behr <julie.behr@choderalab.org>
2014-08-27 15:16:07 -0400

    Multiple equilibration timesteps

--- trustbutverify/mixture_system.py
+++ trustbutverify/mixture_system.py
@@ -20,7 +20,7 @@ import itertools
 
 N_STEPS_MIXTURES = 500 #25000000 # 50 ns
 N_EQUIL_STEPS_MIXTURES = 5000000 # 5ns
-OUTPUT_FREQUENCY_MIXTURES = 500
+OUTPUT_FREQUENCY_MIXTURES = 499 #500
 
 class MixtureSystem(System):
     def __init__(self, cas_strings, n_monomers, temperature, pressure=PRESSURE, output_frequency=OUTPUT_FREQUENCY_MIXTURES, n_steps=N_STEPS_MIXTURES, equil_output_frequency=OUTPUT_FREQUENCY_MIXTURES, **kwargs):
@@ -62,24 +62,7 @@ class MixtureSystem(System):
                 outfile.write(ffxml.read())
                 outfile.close()
                 ffxml.seek(0)
-            ff = app.ForceField(ffxml)
             for k, ligand_traj in enumerate(ligand_trajectories):
-                pos = ligand_traj.openmm_positions(0)
-                top = ligand_traj.top.to_openmm()
-                ligand_traj.unitcell_vectors = np.array([np.eye(3)])
-                top.setUnitCellDimensions(mm.Vec3(*ligand_traj.unitcell_lengths[0]) * u.nanometer)
-                system = ff.createSystem(top, nonbondedMethod=app.NoCutoff, nonbondedCutoff=self.cutoff, constraints=app.HBonds)
-                integrator = mm.LangevinIntegrator(self.temperature, self.equil_friction, self.equil_timestep)
-                simulation = app.Simulation(top, system, integrator, platform=self.platform)
-                simulation.context.setPositions(pos)
-                print('Minimizing Single Molecule')
-                simulation.minimizeEnergy()
-                simulation.context.setVelocitiesToTemperature(self.temperature)
-                with gaff2xml.utils.enter_temp_directory():
-                    ligand_traj.save("initial_top.pdb")
-                    simulation.reporters.append(app.DCDReporter("initial_equil.dcd", 50))
-                    simulation.step(500)
-                    ligand_traj = md.load("./initial_equil.dcd", top="./initial_top.pdb")[-1]
                 pdb_filename = self.monomer_pdb_filenames[k]
                 if not os.path.exists(pdb_filename):
                     ligand_traj.save(pdb_filename)
@@ -108,7 +91,7 @@ class MixtureSystem(System):
         ff = self.ffxml
 
         system = ff.createSystem(topology, nonbondedMethod=app.PME, nonbondedCutoff=self.cutoff, constraints=app.HBonds)
-        integrator = mm.LangevinIntegrator(self.temperature, self.equil_friction, self.equil_timestep)
+        integrator = mm.LangevinIntegrator(self.temperature, self.equil_friction, self.equil_timestep/10.)
         system.addForce(mm.MonteCarloBarostat(self.pressure, self.temperature, self.barostat_frequency))
 
         simulation = app.Simulation(topology, system, integrator, platform=self.platform)
@@ -120,9 +103,19 @@ class MixtureSystem(System):
         simulation.context.setVelocitiesToTemperature(self.temperature)
         print('Equilibrating.')
 
-        simulation.reporters.append(app.DCDReporter(self.equil_dcd_filename, self.equil_output_frequency))
-        simulation.step(self.n_equil_steps)
+        simulation.step(1000)
         
+        simulation.integrator.setStepSize(self.equil_timestep/5.0)
+        simulation.step(1000)
+
+        simulation.integrator.setStepSize(self.equil_timestep/2.0)
+        simulation.step(1000)
+
+        simulation.reporters.append(app.DCDReporter(self.equil_dcd_filename, self.equil_output_frequency))
+        simulation.integrator.setStepSize(self.equil_timestep)
+        simulation.step(self.n_equil_steps)
+
+
         # Re-write a better PDB with correct box sizes.
         traj = md.load(self.equil_dcd_filename, top=self.box_pdb_filename)[-1]
         traj.save(self.equil_pdb_filename)
@@ -156,5 +149,7 @@ class MixtureSystem(System):
         simulation.reporters.append(NonbondedStateDataReporter(self.production_data_filename, self.output_frequency, step=True, potentialEnergy=True, temperature=True, density=True))
         simulation.step(self.n_steps)
 
+        del(simulation)
+        print(os.path.exists(self.production_dcd_filename))
         traj = md.load(self.production_dcd_filename, top=self.equil_pdb_filename)[-1]
         traj.save(self.production_pdb_filename)
